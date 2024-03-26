e9876c70d64c54ee12aa5cf6be33a27d7939b2b6
Farhandika <18220015@std.stei.itb.ac.id>
2024-03-27 03:46:47 +0700

    added sqlx support for pgx

--- app/common.resource.go
+++ app/common.resource.go
@@ -1,10 +1,26 @@
 package app
 
-import "go-be-template/internal/model/config"
+import (
+	_ "database/sql"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/jmoiron/sqlx"
+	"go-be-template/internal/model/config"
+)
 
 type CommonResource struct {
+	Db *sqlx.DB
 }
 
 func NewCommonResource(cfg *config.Config) (*CommonResource, error) {
-	return &CommonResource{}, nil
+	db, err := sqlx.Open(cfg.Sql.Type, cfg.Sql.Host)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return &CommonResource{
+		Db: db,
+	}, nil
 }
